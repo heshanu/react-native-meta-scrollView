70cf7d9e77ed533a3d57044705a842e6af78b8ab
heshanu <heshanu97@gmail.com>
2023-03-17 19:56:40 +0530

    added flatview

--- App.js
+++ App.js
@@ -1,22 +1,23 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
+import * as React from "react";
+import { View, Text } from "react-native";
 
-import LittleLemonHeader from './components/LittleLemonHeader';
-import LittleLemonFooter from './components/LittleLemonFooter';
-import WelcomeScreen from './components/WelcomeScreen';
-
+import LittleLemonHeader from "./components/LittleLemonHeader";
+import LittleLemonFooter from "./components/LittleLemonFooter";
+//import WelcomeScreen from './components/WelcomeScreen';
+import MenuItems from "./components/MenuItems";
 export default function App() {
   return (
     <>
       <View
         style={{
           flex: 1,
-          backgroundColor: '#495E57',
-        }}>
+          backgroundColor: "#495E57",
+        }}
+      >
         <LittleLemonHeader />
-        <WelcomeScreen />
+        <MenuItems />
       </View>
-      <View style={{ backgroundColor: '#495E57' }}>
+      <View style={{ backgroundColor: "#495E57" }}>
         <LittleLemonFooter />
       </View>
     </>
---
+++ components/MenuItems.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import { menuItemsToDisplay } from ".././util/menuItemsToDisplay";
+
+const Item = ({ name, price }) => (
+  <View style={menuStyles.innerContainer}>
+    <Text style={menuStyles.itemText}>{name}</Text>
+    <Text style={menuStyles.itemText}>{price}</Text>
+  </View>
+);
+
+const MenuItems = () => {
+  const renderItem = ({ item }) => <Item name={item.name} price={item.price} />;
+
+  return (
+    <View style={menuStyles.container}>
+      <FlatList
+        data={menuItemsToDisplay}
+        keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+      ></FlatList>
+    </View>
+  );
+};
+
+const menuStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  innerContainer: {
+    paddingHorizontal: 40,
+    paddingVertical: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  itemText: {
+    color: "#F4CE14",
+    fontSize: 20,
+  },
+});
+
+export default MenuItems;
