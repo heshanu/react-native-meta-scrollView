07ae691c668ec9183993e3c2ee5f29c80a5a8e90
heshanu <heshanu97@gmail.com>
2023-03-17 23:49:19 +0530

    Added sectionList

--- App.js
+++ App.js
@@ -1,25 +1,28 @@
-import * as React from "react";
-import { View, Text } from "react-native";
+import * as React from 'react';
+import { View, StyleSheet } from 'react-native';
 
-import LittleLemonHeader from "./components/LittleLemonHeader";
-import LittleLemonFooter from "./components/LittleLemonFooter";
-//import WelcomeScreen from './components/WelcomeScreen';
-import MenuItems from "./components/MenuItems";
+import LittleLemonHeader from './components/LittleLemonHeader';
+import LittleLemonFooter from './components/LittleLemonFooter';
+import MenuItems from './components/MenuItems';
+
 export default function App() {
   return (
     <>
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: "#495E57",
-        }}
-      >
+      <View style={styles.container}>
         <LittleLemonHeader />
         <MenuItems />
       </View>
-      <View style={{ backgroundColor: "#495E57" }}>
+      <View style={styles.footerContainer}>
         <LittleLemonFooter />
       </View>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#333333',
+  },
+  footerContainer: { backgroundColor: '#333333' },
+});
