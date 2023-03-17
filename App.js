64bbfe75d0fcbe00319597ce9f93dece046b37c1
heshanu <heshanu97@gmail.com>
2023-03-17 13:54:59 +0530

    Added viewScroll

--- App.js
+++ App.js
@@ -1,24 +1,17 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
-
+import { View } from 'react-native';
 import LittleLemonHeader from './components/LittleLemonHeader';
-import LittleLemonFooter from './components/LittleLemonFooter';
-import WelcomeScreen from './components/WelcomeScreen';
+import MenuItems from './components/MenuItems';
 
 export default function App() {
   return (
-    <>
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: '#495E57',
-        }}>
-        <LittleLemonHeader />
-        <WelcomeScreen />
-      </View>
-      <View style={{ backgroundColor: '#495E57' }}>
-        <LittleLemonFooter />
-      </View>
-    </>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: '#495E57',
+      }}>
+      <LittleLemonHeader />
+      <MenuItems />
+    </View>
   );
 }
